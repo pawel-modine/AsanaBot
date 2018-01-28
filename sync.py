c9d411952bdae1003ce146f268c1e51a85cb25fb
Ryan May <rmay@ucar.edu>
2018-01-28 02:04:00 -0700

    Fix typo and stupid bug

--- sync.py
+++ sync.py
@@ -112,7 +112,7 @@ class AsanaSync:
         # Find the Asana task that goes with this issue
         try:
             task_id = self.find_task(issue)
-            task = self._client.tasks.update(task, sync_attrs)
+            task = self._client.tasks.update(task_id, sync_attrs)
         except ValueError:
             if issue.state == 'open' and issue.milestone is not None:
                 project = self.find_project(workspace, repo.name)['id']
