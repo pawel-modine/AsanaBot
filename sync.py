56a4e4c6aa88f7080161bd69c428f7333b9bcd93
Ryan May <rmay@ucar.edu>
2018-01-28 01:43:14 -0700

    Refactor syncing to improve the operation

--- sync.py
+++ sync.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 import json
+import os
 import sys
 
 import asana
@@ -18,7 +19,10 @@ def get_asana_client():
     try:
         with open(token_file, 'r') as fobj:
             token = json.load(fobj)
-            return asana.Client.oauth(client_id=ASANA_CLIENT_ID, token=token)
+            return asana.Client.oauth(client_id=ASANA_CLIENT_ID, token=token,
+                                      auto_refresh_url='https://app.asana.com/-/oauth_token',
+                                      auto_refresh_kwargs={'client_id': ASANA_CLIENT_ID, 'client_secret': ASANA_SECRET_ID},
+                                      token_updater=save_token)
     except IOError:
         asana_client = asana.Client.oauth(client_id=ASANA_CLIENT_ID, client_secret=ASANA_SECRET_ID,
                                           redirect_uri='urn:ietf:wg:oauth:2.0:oob')
@@ -45,117 +49,100 @@ def get_issues(org, repo):
             break
 
 
-def parse_payload(request):
-    """Parse GitHub webhook payload to get the updated issue."""
-    raise NotImplementedError
+class AsanaSync:
+    def __init__(self, client):
+        self._client = client
 
+    @lru_cache()
+    def find_workspace(self, org: str):
+        """Find the Asana workspace to go with a GitHub organization."""
+        org = org.lower()
+        for workspace in self._client.workspaces.find_all():
+            if workspace['name'].lower() == org:
+                return workspace
+        else:
+            raise ValueError('Could not find workspace for: {}'.format(org))
 
-@lru_cache()
-def find_workspace(org: str):
-    """Find the Asana workspace to go with a GitHub organization."""
-    org = org.lower()
-    for workspace in asana_client.workspaces.find_all():
-        if workspace['name'].lower() == org:
-            return workspace
-    else:
-        raise ValueError('Could not find workspace for: {}'.format(org))
-
-
-@lru_cache()
-def find_project(workspace: int, repo: str):
-    """Find the project to go with the repository."""
-    repo = repo.lower()
-    for project in asana_client.projects.find_all({'workspace': workspace}):
-        if project['name'].lower() == repo:
-            return project
-    else:
+    @lru_cache()
+    def find_project(self, workspace: int, repo: str):
+        """Find the project to go with the repository."""
+        repo = repo.lower()
+        for project in self._client.projects.find_all({'workspace': workspace}):
+            if project['name'].lower() == repo:
+                return project
         raise ValueError('Could not find appropriate project for: {}'.format(repo))
 
+    @lru_cache()
+    def find_github_tag(self, workspace: int):
+        """Find the GitHub tag on Asana."""
+        tag_name = 'GitHub'
+        for tag in self._client.tags.find_by_workspace(workspace):
+            if tag['name'].lower() == tag_name.lower():
+                break
+        else:  # Did not find one
+            tag = self._client.tags.create_in_workspace(workspace, dict(name=tag_name))
 
-def get_ids(org: str, repo: str):
-    """Get the Asana ids to go with GitHub information."""
-    workspace = find_workspace(org)
-    project = find_project(workspace['id'], repo)
-    return workspace['id'], project['id']
+        return tag['id']
 
+    @lru_cache()
+    def github_to_asana_user(self, workspace: int, github_user: str):
+        """Figure out the Asana user that corresponds to a GitHub user."""
+        for user in self._client.users.find_by_workspace(workspace):
+            if user['name'] == github_user:
+                return user
+        return 'null'
 
-@lru_cache()
-def find_github_tag(workspace: int):
-    """Find the GitHub tag on Asana."""
-    tag_name = 'GitHub'
-    for tag in asana_client.tags.find_by_workspace(workspace):
-        if tag['name'].lower() == tag_name.lower():
-            break
-    else:  # Did not find one
-        tag = asana_client.tags.create_in_workspace(workspace, dict(name=tag_name))
+    def sync_issue(self, issue):
+        """Synchronize a GitHub issue to an Asana task.
 
-    return tag['id']
+        Either create a new task or update attributes of existing task.
+        """
+        repo = issue.repository
+        org = repo.organization
+        workspace = self.find_workspace(org.name)['id']
 
+        sync_attrs = {}
+        if issue.assignee:
+            sync_attrs['assignee'] = self.github_to_asana_user(workspace, issue.assignee.name)
+        else:
+            sync_attrs['assignee'] = 'null'
 
-@lru_cache()
-def github_to_asana_user(workspace: int, github_user: str):
-    """Figure out the Asana user that corresponds to a GitHub user."""
-    for user in asana_client.users.find_by_workspace(workspace):
-        if user['name'] == github_user:
-            return user
-    return 'null'
+        sync_attrs['completed'] = issue.state == 'closed'
 
+        # Find the Asana task that goes with this issue
+        try:
+            task = self.find_task(issue)
+            self._client.tasks.update(task, sync_attrs)
+        except ValueError:
+            if issue.state == 'open' and issue.milestone is not None:
+                project = self.find_project(workspace, repo.name)['id']
+                task = self.create_task(workspace, project, issue, sync_attrs)
 
-def sync_issue(issue):
-    """Synchronize a GitHub issue to an Asana task.
+    def find_task(self, issue):
+        """Find task corresponding to the issue."""
+        try:
+            return self._client.tasks.find_by_id('external:' + issue_to_id(issue))['id']
+        except asana.error.NotFoundError as e:
+            raise ValueError('No task found for issue.') from e
 
-    Either create a new task or update attributes of existing task.
-    """
-    repo = issue.repository
-    org = repo.organization
-    workspace = find_workspace(org.name)['id']
-
-    sync_attrs = {}
-    if issue.assignee:
-        sync_attrs['assignee'] = github_to_asana_user(workspace, issue.assignee.name)
-    else:
-        sync_attrs['assignee'] = 'null'
-
-    sync_attrs['completed'] = issue.state == 'closed'
-
-    # Find the Asana task that goes with this issue
-    try:
-        task = find_task(issue)
-        asana_client.tasks.update(task, sync_attrs)
-    except ValueError:
-        if issue.state == 'open' and issue.milestone is not None:
-            project = find_project(workspace, repo.name)['id']
-            task = create_task(workspace, project, issue, sync_attrs)
-
-
-def find_task(issue):
-    """Find task corresponding to the issue."""
-    try:
-        return asana_client.tasks.find_by_id('external:' + issue_to_id(issue))['id']
-    except asana.error.NotFoundError as e:
-        raise ValueError('No task found for issue.') from e
-
+    def create_task(self, workspace: int, project: int, issue, attrs: dict):
+        """Create a task corresponding to a GitHub issue."""
+        github_tag = self.find_github_tag(workspace)
+        params = {'external': {'id': issue_to_id(issue)},
+                  'name': '{0.title} (#{0.number})'.format(issue),
+                  'notes': '\n\n'.join((issue.html_url, issue.body)),
+                  'projects': [project],
+                  'tags': [github_tag]}
+        params.update(attrs)
+        return self._client.tasks.create_in_workspace(workspace, params)
 
 def issue_to_id(issue):
     """Create a unique id from an issue."""
     return '{0.repository.organization.name}-{0.repository.name}-{0.number:d}'.format(issue)
 
-
-def create_task(workspace: int, project: int, issue, attrs: dict):
-    """Create a task corresponding to a GitHub issue."""
-    github_tag = find_github_tag(workspace)
-    params = {'external': {'id': issue_to_id(issue)},
-              'name': '{0.title} (#{0.number})'.format(issue),
-              'notes': '\n\n'.join((issue.html_url, issue.body)),
-              'projects': [project],
-              'tags': [github_tag]}
-    params.update(attrs)
-    return asana_client.tasks.create_in_workspace(workspace, params)
-
-
 if __name__ == '__main__':
-    asana_client = get_asana_client()
-    github_client = github.Github(GITHUB_TOKEN)
+    syncer = AsanaSync(get_asana_client())
+    github_client = github.Github(os.environ.get('GITHUB_TOKEN'))
 
     rate = github_client.get_rate_limit().rate
     print('API calls remaining: {0} (Resets at {1})'.format(rate.remaining, rate.reset))
@@ -164,4 +151,4 @@ if __name__ == '__main__':
     repo = 'MetPy'
 
     for issue in get_issues(org, repo):
-        sync_issue(issue)
+        syncer.sync_issue(issue)
