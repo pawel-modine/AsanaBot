70ff60a90265cc6b2a3fc90697586ec8678662eb
Ryan May <rmay@ucar.edu>
2018-02-04 12:14:37 -0700

    Accept only requests with the proper signature

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import hashlib
+import hmac
 import json
 import logging
 import os
 import os.path
+import secrets
 
 import asana
-from chalice import Chalice
+from chalice import Chalice, UnauthorizedError
 
 from chalicelib.sync import AsanaSync, IssueInfo
 
@@ -25,6 +28,7 @@ def github_setup():
 
 @app.route('/hooks/github', methods=['POST'])
 def sync():
+    check_signature(app.current_request)
     try:
         task = {}
         body = app.current_request.json_body
@@ -38,6 +42,21 @@ def sync():
         app.log.exception('Exception:', exc_info=e)
         raise e
     return task
+
+
+def check_signature(request):
+    if 'X-HUB-SIGNATURE' not in request.headers:
+        raise UnauthorizedError('Missing X-HUB-SIGNATURE header.')
+
+    alg, header_sig = request.headers['X-HUB-SIGNATURE'].split('=')
+    digest = hmac.HMAC(os.environ['GITHUB_WEBHOOK_SECRET'].encode('ascii'),
+                       request.raw_body, digestmod=getattr(hashlib, alg))
+    sig = digest.hexdigest()
+    app.log.debug('Comparing signatures. Got: %s Calculated: %s', header_sig, sig)
+
+    if not secrets.compare_digest(header_sig, sig):
+        raise UnauthorizedError('Signatures do not match.')
+
 
 def get_asana_client():
     """Handle the details of setting up OAUTH2 access to Asana."""
